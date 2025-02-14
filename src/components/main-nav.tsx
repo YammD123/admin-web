89ff2fcd9edc0d095fa34bcafd8fd880bf2bc5a8
YammD123 <siamqueen52@gmail.com>
2025-02-15 00:23:01 +0700

    main navbar dengan params

---
+++ src/components/main-nav.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+import React from "react";
+
+export default function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const pathname = usePathname();
+  const params = useParams();
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
+  return (
+    <nav className={cn(`flex items-center space-x-4 lg:space-x-6`, className)}>
+      {routes.map((route: any) => {
+        return (
+          <Link href={route.href} key={route.href} className={cn(``)}>
+            {route.label}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+import MainNav from './main-nav'
+
+export default function Navbar() {
+  return (
+    <div className='border-b'>
+        <div className='flex h-16 items-center px-4'>
+            <div>Store Switcher</div>
+            <MainNav/>
+            <div className='ml-auto flex items-center space-x-4'>
+                <UserButton afterSignOutUrl='/'/>
+            </div>
+        </div>
+    </div>
+  )
+}
